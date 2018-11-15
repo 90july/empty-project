98ae1dc5e513fb7700f0a0dea6e9c0810cd3dec1
ariel <470257271@qq.com>
2018-11-15 16:46:25 +0800

    commit service-01

---
+++ consumer-01/src/main/java/net/sunnada/consumer/Consumer01Application.java
@@ -0,0 +1,12 @@
+package net.sunnada.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumer01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consumer01Application.class, args);
+	}
+}
